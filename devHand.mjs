8f4d6042f74b5d6300634b832632b4716f81be2e
Nikhil Jain <nik.jain@gmail.com>
2024-01-11 23:57:53 +0530

    minor tweaks

--- devHand.mjs
+++ devHand.mjs
@@ -19,21 +19,23 @@ async function init() {
     process.exit(1);
   }
 
-  const functionName = args[0];
+  const cmd = args[0];
 
   // Check if command is alphanumeric
-  if (!/^[a-zA-Z0-9]+$/.test(functionName)) {
+  if (!/^[a-zA-Z0-9]+$/.test(cmd)) {
     console.error('Error: value should only be alphanumeric');
     process.exit(1);
   }
 
-
+  const basicFunctionsAliases = new Map();
+  basicFunctionsAliases.set('date', 'time');
+  console.log(basicFunctionsAliases.get(cmd), cmd);
   // Execute the specified function with the provided parameter
-  if (basicFunctions[functionName]) {
-    const result = await basicFunctions[functionName](...args.slice(1));
+  if (basicFunctions[cmd] || basicFunctions[basicFunctionsAliases.get(cmd)]) {
+    const result = await basicFunctions[cmd](...args.slice(1));
     console.log(result);
   } else {
-    console.error(`Error: Function ${functionName} not found`);
+    console.error(`Error: Function ${cmd} not found`);
     process.exit(1);
   }
 }
